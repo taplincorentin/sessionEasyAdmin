c707120e2e34dd32d673a722befdb0aec1b4bb06
Corentin Taplin <taplincorentin@gmail.com>
2023-08-30 15:15:36 +0200

    changed field type in programme for form

--- src/Controller/Admin/ProgrammeCrudController.php
+++ src/Controller/Admin/ProgrammeCrudController.php
@@ -3,12 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Programme;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProgrammeCrudController extends AbstractCrudController
@@ -22,8 +22,8 @@ class ProgrammeCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('session'),
-            TextField::new('module'),
+            AssociationField::new('session'),
+            AssociationField::new('module'),
             IntegerField::new('nbJours'),
         ];
     }
